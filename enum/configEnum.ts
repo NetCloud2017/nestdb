3bf48b738fd614d9213335155e3f1afccb9fb03b
NetCloud2017 <tothinktodo@163.com>
2025-03-21 22:59:10 +0800

    添加环境配置文件及配置校验，创建数据实体连接数据库

---
+++ enum/configEnum.ts
@@ -0,0 +1,11 @@
+export enum ConfigEnum {
+  DB = 'DB',
+  DB_HOST = 'DB_HOST',
+  DB_TYPE = 'DB_TYPE',
+  DB_PASSWORD = 'DB_PASSWORD',
+  DB_USERNAME = 'DB_USERNAME',
+  DB_DATABASE = 'DB_DATABASE',
+  DB_PORT = 'DB_PORT',
+
+  DB_SYNC = 'DB_SYNC',
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,65 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+import * as Joi from 'joi';
+import { ConfigEnum } from 'enum/configEnum';
+
+import { User } from './user/user.entity';
+
+const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
+console.log(envFilePath, 'pths');
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 8088,
-      username: 'postgres',
-      password: '123',
-      database: 'postgresdb',
-      entities: [],
-      synchronize: true,
-      logging: ['error'],
+    ConfigModule.forRoot({
+      load: [() => dotenv.config({ path: '.env' })],
+      isGlobal: true,
+      envFilePath,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        DB_HOST: Joi.string().ip(),
+        DB_URL: Joi.string().domain(),
+        DB_PORT: Joi.number().default(8088),
+        DB_TYPE: Joi.string().valid('mysql', 'postgres'),
+        DB_DATABASE: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_SYNC: Joi.boolean().default(false),
+      }),
+    }),
+
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 8088,
+    //   username: 'postgres',
+    //   password: '123',
+    //   database: 'postgresdb',
+    //   entities: [],
+    //   synchronize: true,
+    //   logging: ['error'],
+    // }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        console.log(configService.get(ConfigEnum.DB_DATABASE), 'database');
+        return {
+          type: configService.get(ConfigEnum.DB_TYPE),
+          host: configService.get(ConfigEnum.DB_HOST),
+          port: configService.get(ConfigEnum.DB_PORT),
+          username: configService.get(ConfigEnum.DB_USERNAME),
+          password: configService.get(ConfigEnum.DB_PASSWORD),
+          database: configService.get(ConfigEnum.DB_DATABASE),
+          entities: [User],
+          synchronize: configService.get(ConfigEnum.DB_SYNC),
+          logging: ['error'],
+        } as TypeOrmModuleOptions;
+      },
     }),
   ],
   controllers: [AppController],
