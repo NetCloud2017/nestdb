7f01162b7a5d2b57575106b3e7daf7761466149f
NetCloud2017 <tothinktodo@163.com>
2025-03-28 10:56:26 +0800

    添加注射

--- src/logs/logs.service.ts
+++ src/logs/logs.service.ts
@@ -1,41 +1,40 @@
-import { Logs } from "./logs.entity";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Injectable } from "@nestjs/common";
-import { Repository } from "typeorm";
-import { User } from "../user/user.entity";
+import { Logs } from './logs.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from '../user/user.entity';
 
 @Injectable()
 export class LogsService {
-  constructor (
-    @InjectRepository(Logs) 
+  constructor(
+    @InjectRepository(Logs)
     private readonly logsRepository: Repository<Logs>,
 
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>
+    private readonly userRepository: Repository<User>,
   ) {}
-  findOne (id: string) {
+  findOne(id: string) {
     return this.userRepository.findOne({
       where: {
         id,
-      }
-    })
-  }
- async findUserLogs(id: string) {
-    const user = await this.findOne(id) 
-    return this.logsRepository.find({
-      where: {
-        id,
       },
+    });
+  }
+  async findUserLogs(id: string) {
+    const user = await this.findOne(id);
+    return this.logsRepository.find({
+      // 打开报错
+      // where: {user},
       relations: {
         user: true,
-      }
-    })
+      },
+    });
   }
 
   findLogsByGroup(id: string) {
     return this.logsRepository.query(
-      'SELECT logs.result as rest, COUNT(logs.result) as count from logs, user WHERE user.id = logs.userId AND user.id = 2 GROUP BY logs.result'
-    )
+      'SELECT logs.result as rest, COUNT(logs.result) as count from logs, user WHERE user.id = logs.userId AND user.id = 2 GROUP BY logs.result',
+    );
     // return this.logsRepository
     //   .createQueryBuilder('logs')
     //   .select('logs.result', 'result')
